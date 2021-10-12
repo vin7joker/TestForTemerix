b72d716125bbbcb24d8cf18011e1b1308d36d41c
vin7joker <02oleh.semeniuk@gmail.com>
2021-10-12 23:54:08 +0300

    Пофиксил неисправности с входом в каталог после обновления сайта в понедельник

--- src/test/java/page/MainPage.java
+++ src/test/java/page/MainPage.java
@@ -10,8 +10,9 @@ import static com.codeborne.selenide.Selenide.*;
 public class MainPage extends BasePage{
     private final String SITE_URL = "https://shoptool.com.ua/";
     private SelenideElement sectionOfDiscounts = $x("//*[text()='Скидки']");
-    private SelenideElement CATALOG = $(By.id("sw_dropdown_3626"));
-    private String POWER_TOOL= "a[href='https://shoptool.com.ua/elektroinstrument/'] > .menu-lvl-ctn ";
+    private SelenideElement catalog = $(By.id("sw_dropdown_3626"));
+    private SelenideElement catalog_view = $(By.id("dropdown_3626"));
+    private String power_tool = "(//span[@class='menu-lvl-ctn '])[18]";
 
     //Метод открывает сайт
     public MainPage openSite(){
@@ -27,8 +28,11 @@ public class MainPage extends BasePage{
     /*Метод наводит мышь на Каталог товаров чтобы появились скрытие элементы.
      После этого нажимает на раздел Электроинстументы. Потом переходит в заданный строкой раздел.*/
     public void goToSection(String section){
-        CATALOG.scrollTo();
-        CATALOG.hover().find(POWER_TOOL).shouldHave(text("Электроинструмент")).hover().$(POWER_TOOL).click();
+        catalog.scrollTo();
+        moveToElement(catalog);
+        waitOneSecond();
+        moveToElement(catalog_view);
+        $x(power_tool).click();
         String selector = "//a[text()='"+section+"']";
         boolean isSelecor = $x(selector).isDisplayed();
         Assert.assertTrue(isSelecor,"Вы неправильно ввели название раздела");
